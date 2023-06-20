33f68fb676b9a588e690daf1f8115fc5a37994b4
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2023-06-20 10:21:02 -0400

    add script to auto detect wav formatting for wav support

--- demo/audio_stream_player.gd
+++ demo/audio_stream_player.gd
@@ -43,10 +43,15 @@ func play_file(file_name: String):
 		mp3_stream.loop = true
 		_play_stream(mp3_stream, false)
 	elif file_name.ends_with(".wav"):
+		var wav_formatting : Array = _check_wav_format(data)  # Format of returned array is format code, sample rate, channel number
 		var wav_stream = AudioStreamWAV.new()
 		wav_stream.data = data
-		wav_stream.format = AudioStreamWAV.FORMAT_16_BITS
-		wav_stream.mix_rate = 22050
+		wav_stream.format = wav_formatting[0]#AudioStreamWAV.FORMAT_16_BITS
+		wav_stream.mix_rate = wav_formatting[1]#22050
+		if wav_formatting[2] == 2:
+			wav_stream.stereo = true
+		else:
+			wav_stream.stereo = false
 		wav_stream.loop_mode = AudioStreamWAV.LOOP_DISABLED
 		_play_stream(wav_stream, false)
 
@@ -60,3 +65,52 @@ func _play_stream(audio_stream: AudioStream, mute_bus: bool):
 	
 	# Play the stream
 	play()
+
+# Code to check .wav file formatting; code is adapted from https://github.com/Gianclgar/GDScriptAudioImport/blob/master/GDScriptAudioImport.gd
+func _check_wav_format(data):
+	var bits_per_sample = 0
+	var format_code
+	var format_name
+	var sample_rate
+	var channel_num
+
+	for i in range(0, 100):
+		var those4bytes = str(char(data[i])+char(data[i+1])+char(data[i+2])+char(data[i+3]))
+		
+		if those4bytes == "RIFF": 
+			print ("RIFF OK at bytes " + str(i) + "-" + str(i+3))
+			#RIP bytes 4-7 integer for now
+		if those4bytes == "WAVE": 
+			print ("WAVE OK at bytes " + str(i) + "-" + str(i+3))
+
+		if those4bytes == "fmt ":
+			print ("fmt OK at bytes " + str(i) + "-" + str(i+3))
+			
+			#get format subchunk size, 4 bytes next to "fmt " are an int32
+			var formatsubchunksize = data[i+4] + (data[i+5] << 8) + (data[i+6] << 16) + (data[i+7] << 24)
+			print ("Format subchunk size: " + str(formatsubchunksize))
+			
+			#using formatsubchunk index so it's easier to understand what's going on
+			var fsc0 = i+8 #fsc0 is byte 8 after start of "fmt "
+
+			#get format code [Bytes 0-1]
+			format_code = data[fsc0] + (data[fsc0+1] << 8)
+			if format_code == 0: format_name = "8_BITS"
+			elif format_code == 1: format_name = "16_BITS"
+			elif format_code == 2: format_name = "IMA_ADPCM"
+			else: 
+				format_name = "UNKNOWN (trying to interpret as 16_BITS)"
+				format_code = 1
+			print ("Format: " + str(format_code) + " " + format_name)
+
+			
+			#get channel num [Bytes 2-3]
+			channel_num = data[fsc0+2] + (data[fsc0+3] << 8)
+			print ("Number of channels: " + str(channel_num))
+			
+			#get sample rate [Bytes 4-7]
+			sample_rate = data[fsc0+4] + (data[fsc0+5] << 8) + (data[fsc0+6] << 16) + (data[fsc0+7] << 24)
+			print ("Sample rate: " + str(sample_rate))
+
+			
+	return [format_code, sample_rate, channel_num]
